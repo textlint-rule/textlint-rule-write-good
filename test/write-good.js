3c12535a7e03a9d1f801dc3d0174b9cb3a555e19
nodaguti <nodaguti@gmail.com>
2016-03-17 00:19:13 +0900

    Initial commit

---
+++ test/write-good.js
@@ -0,0 +1,34 @@
+import rule from '../src/write-good';
+import TextlintTester from 'textlint-tester';
+
+const tester = new TextlintTester();
+
+tester.run('write-good', rule, {
+  valid: [
+    'Hello, world!',
+    'This sentence contains no mistakes.',
+    {
+      text: 'The cat was stolen.',
+      options: {
+        passive: false,
+      },
+    },
+  ],
+  invalid: [
+    {
+      text: 'So the cat was stolen.',
+      errors: [
+        {
+          message: '"So" adds no meaning',
+          line: 1,
+          column: 1,
+        },
+        {
+          message: '"was stolen" may be passive voice',
+          line: 1,
+          column: 12,
+        },
+      ],
+    },
+  ],
+});
