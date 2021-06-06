766e5cf688a4c600b4d6d45ef75c0a29ab330a37
azu <azuciao@gmail.com>
2021-06-06 11:38:03 +0900

    refactor: update dependencies

--- test/write-good.js
+++ test/write-good.js
@@ -1,34 +1,34 @@
-import TextlintTester from 'textlint-tester';
-import rule from '../src/write-good';
+import TextlintTester from "textlint-tester";
+import rule from "../src/write-good";
 
 const tester = new TextlintTester();
 
-tester.run('write-good', rule, {
-  valid: [
-    'Hello, world!',
-    'This sentence contains no mistakes.',
-    {
-      text: 'The cat was stolen.',
-      options: {
-        passive: false,
-      },
-    },
-  ],
-  invalid: [
-    {
-      text: 'So the cat was stolen.',
-      errors: [
+tester.run("write-good", rule, {
+    valid: [
+        "Hello, world!",
+        "This sentence contains no mistakes.",
         {
-          message: '"So" adds no meaning',
-          line: 1,
-          column: 1,
-        },
+            text: "The cat was stolen.",
+            options: {
+                passive: false
+            }
+        }
+    ],
+    invalid: [
         {
-          message: '"was stolen" may be passive voice',
-          line: 1,
-          column: 12,
-        },
-      ],
-    },
-  ],
+            text: "So the cat was stolen.",
+            errors: [
+                {
+                    message: '"So" adds no meaning',
+                    line: 1,
+                    column: 1
+                },
+                {
+                    message: '"was stolen" may be passive voice',
+                    line: 1,
+                    column: 12
+                }
+            ]
+        }
+    ]
 });
